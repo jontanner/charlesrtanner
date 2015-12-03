fd5e0f2f9a41073e17154919352bd6bca8bcc993
jontanner <jon.james.tanner@gmail.com>
2015-12-02 17:33:52 -0800

    First iteration of home page stuff

--- javascripts/menu.js
+++ javascripts/menu.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	var menu = $('.menu');
 	var menuIcon = $('.menu-icon');
   var subMenuToggleSelectors = $('.published-menu, .unpublished-menu');
+  var stickyMenuSelector = $('.sticky-bottom-menu');
   var menuSpeed = 300
 
 	menuIcon.click(function() {
@@ -26,9 +27,11 @@ $(document).ready(function() {
 
   $(document).scroll(function(){
     if($(document).scrollTop() > $('header').outerHeight()) {
-      menuIcon.css('background-color' , 'rgba(0,0,0,0.5)')
+      menuIcon.css('background-color' , 'rgba(0,0,0,0.5)'),
+      stickyMenuSelector.css('bottom' , '0px')
     } else {
       menuIcon.css('background-color' , 'rgba(255,255,255,0.5)')
+      stickyMenuSelector.css('bottom' , '-100px')
     }
   })
 
@@ -48,5 +51,23 @@ $(document).ready(function() {
     subMenuToggleSelectors.animate({
       right: "-250px"
     }, menuSpeed)
+  })
+
+  $('.about').click(function() {
+    $('html, body').animate({
+      scrollTop: $('#about').offset().top 
+    }, 800);
+  })
+
+  $('.authors-note').click(function() {
+    $('html, body').animate({
+      scrollTop: $('#authors-note').offset().top 
+    }, 800);
+  })
+
+  $('.contact').click(function() {
+    $('html, body').animate({
+      scrollTop: $('#contact').offset().top 
+    }, 800);
   })
 });
